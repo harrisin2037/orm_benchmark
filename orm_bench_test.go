004a66e2976daa78472cc955bd4610b640d5bd18
harrisin2037 <harrisin2037@gmail.com>
2021-12-30 08:47:10 +0800

    testing main func for dynami cases

--- orm_bench_test.go
+++ orm_bench_test.go
@@ -1,6 +1,7 @@
 package orm_benchmark
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -16,38 +17,51 @@ var allORMs = map[string]func() BenchORMInstance{
 	"pgx": PgxTestBenchORMInstance,
 }
 
-func TestMain(m *testing.M) {
-	godotenv.Load(".env")
-}
+/*
+	As testing main will never return as it calls os.Exit(), all benchmark tests cannot be run,
+	so need to assembling the dynamic benchmark cases (values of struct testing.InternalBenchmark,
+	ref: https://pkg.go.dev/testing#InternalBenchmark), call testing.Main() (ref: https://pkg.go.dev/testing#Main)
+	which properly parses command line flags, creates and sets up testing.M (ref: https://pkg.go.dev/testing#M),
+	and prepares and calls testing.RunBenchmarks() (ref: https://pkg.go.dev/testing#RunBenchmarks).
+	This way your dynamic benchmarks will still be runnable by go test.
 
-func BenchmarkORMsTestWithCases(b *testing.B) {
+	Notes: testing.Main() will never return as it calls os.Exit().
+	If you want to perform further logging and calculations on the benchmark results,
+	you may also call testing.MainStart().Run() (which is what testing.Main() does), 
+	and you may pass the exit code which is returned by M.Run() to os.Exit().
+*/
+func TestMain(m *testing.M) {
 
 	type Test struct {
-		run func(b *testing.B, ins BenchORMInstance)
+		name string
+		run  func(b *testing.B, ins BenchORMInstance)
 	}
+
+	godotenv.Load(".env")
 
 	tests := []Test{
-		{testConn},
+		{"connection test", testConn},
 	}
 
-	for i := 0; i < len(tests); i++ {
-		benchTestORMs(b, tests[i].run)
-	}
-}
+	benchmarks := []testing.InternalBenchmark{}
 
-func ORMsTestConn(b *testing.B) {
-	benchTestORMs(b, testConn)
+	for _, test := range tests {
+		for ormName, ormInstance := range allORMs {
+			bm := testing.InternalBenchmark{
+				Name: fmt.Sprintf("%T[test case = %s, orm = %s]", test, test.name, ormName),
+				F: func(b *testing.B) {
+					b.ResetTimer()
+					test.run(b, ormInstance())
+				},
+			}
+			benchmarks = append(benchmarks, bm)
+		}
+	}
+	anything := func(pat, str string) (bool, error) { return true, nil }
+
+	testing.Main(anything, nil, benchmarks, nil)
 }
 
 func testConn(b *testing.B, ins BenchORMInstance) {
 	ins.ConnTest(b)
-}
-
-func benchTestORMs(b *testing.B, f ORMTestingBenchFunc) {
-	for ormName, ormInstance := range allORMs {
-		b.Run(ormName, func(b *testing.B) {
-			b.ResetTimer()
-			f(b, ormInstance())
-		})
-	}
 }
--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -eux
 trap 'docker-compose down -v' EXIT
+
 docker-compose down -v
 docker-compose up -d
+
 sleep 10
-CGO_ENABLED=0 TZ='Asia/Hong_Kong' go test
+
+CGO_ENABLED=0 TZ='Asia/Hong_Kong' go test -run=^$ -bench=. -benchmem -count=1 -cpu 4 | tee bench-purego.txt
+CGO_ENABLED=1 TZ='Asia/Hong_Kong' go test -run=^$ -bench=. -benchmem -count=1 -cpu 4 | tee bench-cgo.txt
+
+benchstat bench-cgo.txt bench-purego.txt
